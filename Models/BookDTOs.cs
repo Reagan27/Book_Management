a4fbfad290d26f58ec2fc7caab3d8e8bf2789ed2
Reagan27 <luttareagan22@gmail.com>
2023-09-15 12:05:48 +0300

    commit

---
+++ Models/BookDTOs.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_Management_Form.Models
+{
+    public class BookDTOs
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> Authors { get; set; } = new List<string>();
+    }
+}
